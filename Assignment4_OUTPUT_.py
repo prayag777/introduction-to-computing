d203b8511db66b7fdda06a4f21a7006925aa9d56
prayag777 <97182747+prayag777@users.noreply.github.com>
2022-02-27 15:47:56 +0530

    Add files via upload

---
+++ Assignment4_OUTPUT_.py
@@ -0,0 +1,112 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Dell\AppData\Local\Programs\Python\Python310\Assignment4_21105014.py
+
+[Question 1]
+
+Enter number of disk in tower of Hanoi:3
+
+The Disks are numbered starting from top of the tower.
+Steps to move all disks from Source Tower to Destination Tower is given below:
+
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 3 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+
+[Question.2](Recursive)
+The Pascle's Triangle using recursive approach is:
+
+Enter number of rows in Pascle's Triangle:12
+                                                 1                                                  
+                                                1  1                                                
+                                              1  2  1                                               
+                                             1  3  3  1                                             
+                                           1  4  6  4  1                                            
+                                         1  5  10  10  5  1                                         
+                                       1  6  15  20  15  6  1                                       
+                                     1  7  21  35  35  21  7  1                                     
+                                   1  8  28  56  70  56  28  8  1                                   
+                                1  9  36  84  126  126  84  36  9  1                                
+                           1  10  45  120  210  252  210  120  45  10  1                            
+                         1  11  55  165  330  462  462  330  165  55  11  1                         
+
+[Question.2](Iterative)
+The Pascle's Triangle using iterative approach is:
+
+Enter number of rows:12
+                                                 1                                                  
+                                                1  1                                                
+                                              1  2  1                                               
+                                             1  3  3  1                                             
+                                           1  4  6  4  1                                            
+                                         1  5  10  10  5  1                                         
+                                       1  6  15  20  15  6  1                                       
+                                     1  7  21  35  35  21  7  1                                     
+                                   1  8  28  56  70  56  28  8  1                                   
+                                1  9  36  84  126  126  84  36  9  1                                
+                           1  10  45  120  210  252  210  120  45  10  1                            
+                         1  11  55  165  330  462  462  330  165  55  11  1                         
+
+[Question.3]
+
+Enter an Integer:2345
+Enter another Integer:23
+
+The quotient when 2345 is divided by 23 is 101.
+The remainder when 2345 is divided by 23 is 22.
+
+Que3.a
+Function is Not-callable
+
+Que3.b
+All result values are 'non-zero'
+
+Que3.c
+Appended (4,5,6) in the values (101,22)
+Values greater than 4 is 101,22,5,6
+
+Que3.d
+Above result in set form is shown below:
+{5, 101, 22, 6}
+
+Que3.e
+The above set has been converted to immutable.
+
+Que3.f
+Max value from set is 101
+Hash value of 101 is 101
+
+[Question.4]
+
+Hello, My name is Prayag and my roll no. is 21105014
+Destructor Called
+
+[Question.5]
+
+Employee Name is Mehak and Salary is 40000 
+Employee Name is Ashok and Salary is 50000 
+Employee Name is Viren and Salary is 60000 
+
+Que.5a
+Mehak salary Updated to 70000
+Employee Name is Mehak and Salary is 70000 
+
+Que.5b
+Employee Viren's data has been removed.
+
+[Question 6]
+
+          WELCOME TO THE FRIENDSHIP GAME
+
+Enter Player1 name:George
+Enter Player2 name:Barbie
+
+Enter Word by George:aeroplane
+Enter Word by Barbie:planeaero
+
+Friendship of George and Barbie is REAL
